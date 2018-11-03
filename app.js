1e080e1b7c75cf7d659816cb58d96b00cc20b1d5
vlacho13 <striuncekf@hotmail.com>
2018-11-03 00:15:57 -0500

    added response url /calc link

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 var http = require("http");
 var port = 9000;
 
-http.createServer(function(req, resp) {
-    resp.writeHead(200, {"Content-Type": "text/html"});
+http
+  .createServer(function(req, resp) {
+    console.log(req.url);
+    switch (req.method) {
+      case "GET":
+        if (req.url === "/") {
+          resp.writeHead(200, { "Content-Type": "text/html" });
+          resp.write("<html><head><title>Home</title></head><body>Click <a href='/calc'>here</a> for a calculation</body></html>");
+          resp.end();
+        }
+        break;
+      case "POST":
+        break;
+      default:
+        break;
+    }
+    
+    /*
+    resp.writeHead(200, { "Content-Type": "text/html" });
     resp.write("<html><body>Hello <strong><i>World</i></strong></body></html>");
     resp.end();
-}).listen(port);
+    */
+  })
+  .listen(port);
